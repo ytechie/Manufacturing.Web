46d688a64e2e0f9b1ae8fa658e5183d257c2f48e
Jason Young <jason@ytechie.com>
2014-11-05 13:39:11 -0600

    Moved the URL configuration to a js file

--- src/app/manufacturingWeb.js
+++ src/app/manufacturingWeb.js
@@ -1,3 +1,10 @@
 'use strict';
 
-angular.module('manufacturingWeb', ['ngRoute', 'ngAnimate']);
+angular.module('manufacturingWeb', ['ngRoute']);
+
+angular.module('manufacturingWeb').factory('config', function () {
+    return {
+        //apiUrl: "http://192.168.1.202/opcweb"
+        apiUrl: "http://opcgate.azurewebsites.net"
+    }
+});
