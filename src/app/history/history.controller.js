6407de9ac36da058205d8e2f07065e31a11867db
Jason Young <jason@ytechie.com>
2014-11-05 13:39:40 -0600

    Removed URL hardcoding

--- src/app/history/history.controller.js
+++ src/app/history/history.controller.js
@@ -1,14 +1,14 @@
 ﻿angular.module('manufacturingWeb')
     .controller('history', [
-        '$scope', '$http', function($scope, $http) {
+        '$scope', '$http', 'config', function($scope, $http, config) {
             $scope.parameterUpdate = function() {
                 //$scope.startDate
 
                 var getConfig = {
-                    url: "http://localhost:16683/api/history", //todo: unhardcode this
+                    url: config.apiUrl + "/api/history",
                     method: "GET",
                     params: {}
-                }
+                };
 
                 if ($scope.startDate) {
                     getConfig.params.startTime = $scope.startDate.toISOString();
